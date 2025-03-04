2207a6550e7287dc8a46e5b35bcf21cd48d825b5
lines14 <tonybass1414@yahoo.com>
2025-03-04 10:46:55 +0500

    Update base_API_user.py

--- main/utils/user/base_API_user.py
+++ main/utils/user/base_API_user.py
@@ -8,7 +8,7 @@ load_dotenv(override=True)
 
 class BaseAPIUser(HttpUser):
     abstract = True
-    host = getenv('GATEWAY_URL')
+    host = getenv('BASE_URL')
     wait_time = between(
         JSONLoader.config_data.min_wait_time, 
         JSONLoader.config_data.max_wait_time
@@ -17,11 +17,11 @@ class BaseAPIUser(HttpUser):
     @events.request.add_listener
     def log_request(name, request_type, response_time, response, exception, context, **kwargs):
         if exception:
-            Logger.error(f"[req] ▶ {request_type}: {getenv('GATEWAY_URL')}{name}")
+            Logger.error(f"[req] ▶ {request_type}: {getenv('BASE_URL')}{name}")
             Logger.error(f"[res]   body: {exception}")
             Logger.error("failed ❌")
         else:
-            print(f"[req] ▶ {request_type}: {getenv('GATEWAY_URL')}{response.url}")
+            print(f"[req] ▶ {request_type}: {getenv('BASE_URL')}{response.url}")
             print(f"[res]   response time: {response_time}ms")
             print(f"[res]   status code: {response.status_code}")
             print("passed ✅")
